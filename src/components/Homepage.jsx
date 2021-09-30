9292ca1e1efdb9137727176868e111f3edf663ca
43695512 <yanter@wp.pl>
2021-09-30 14:55:58 +0200

    Cryptocurrencies Display

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,11 +3,12 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/crypto-api";
+import { Cryptocurrencies, News } from ".";
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   console.log(data, "data");
@@ -56,6 +57,16 @@ const Homepage = () => {
           <Link to="/cryptocurrencies">Show More</Link>
         </Title>
       </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </Fragment>
   );
 };
